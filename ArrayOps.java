7bab39c301cf9ffc63d588b26eb2b6d65da15ac2
Jun <jkubo20@stuy.edu>
2020-10-25 20:23:08 -0400

    largest

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,7 +1,31 @@
-public static int sum(int[] arr) {
-  int total = 0;
-  for (int i = 0; i < arr.length; i++) {
-    total += arr[i];
+class ArrayOps {
+
+  public static int sum(int[] arr) {
+    int total = 0;
+    for (int i = 0; i < arr.length; i++) {
+      total += arr[i];
+    }
+    return total;
   }
-  return total;
+  public static  int largest(int[]arr) {
+    int largest = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > largest) {
+        largest = arr[i];
+      }
+    }
+    return largest;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 }
